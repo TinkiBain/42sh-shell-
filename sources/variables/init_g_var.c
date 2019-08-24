c9362ae87c945547f8a36e9ad3bb8522ea9977b7
ZaprAlex <ggwin-go@student.21-school.ru>
2019-08-24 17:55:00 +0300

    copy environ to g_var; check default var existence before set;

--- sources/variables/init_g_var.c
+++ sources/variables/init_g_var.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/16 20:28:29 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/08/23 17:15:21 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/08/24 16:37:38 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,38 @@
 #include "variables.h"
 #include "defs.h"
 
-int		init_g_var(void)
+static void	set_var_if_not_exist(char *var, char ***vars, int change_readolnly)
 {
+	char	*p;
+
+	if ((p = ft_strchr(var, '=')) && !ft_getenv(var, *vars, p - var))
+		set_var(var, vars, change_readolnly);
+}
+
+int			init_g_var(void)
+{
+	extern char		**environ;
 	extern char		**g_var;
 	char			*tmp;
 
 	if (g_var)
 		ft_free_double_ptr_arr((void ***)&g_var);
-	g_var = (char **)ft_xmalloc(sizeof(char *));
-	ft_bzero(g_var, sizeof(char *));
-	set_var("HISTPATH=~/.42sh.history", &g_var, 1);
-	set_var("HISTSIZE=20", &g_var, 1);
-	set_var("LOGPATH=~/.42sh.log", &g_var, 1);
-	set_var("PS1=" "\\033[0;31m" PROJECT_NAME ">\\033[0m ", &g_var, 1);
-	set_var("PS2=> ", &g_var, 1);
+	if (environ)
+		g_var = create_copy_env(environ);
+	else
+	{
+		g_var = (char **)ft_xmalloc(sizeof(char *));
+		ft_bzero(g_var, sizeof(char *));
+	}
+	set_var_if_not_exist("HISTPATH=~/.42sh.history", &g_var, 1);
+	set_var_if_not_exist("HISTSIZE=20", &g_var, 1);
+	set_var_if_not_exist("LOGPATH=~/.42sh.log", &g_var, 1);
+	set_var_if_not_exist("PS1=" "\\033[0;31m" PROJECT_NAME ">\\033[0m ", &g_var, 1);
+	set_var_if_not_exist("PS2=> ", &g_var, 1);
 	tmp = ft_strrejoin("SHELLHOME=", getcwd(NULL, 1024), 2);
 	set_var(tmp, &g_var, 1);
 	ft_memdel((void **)&tmp);
 	set_var("SHELLOPTS=", &g_var, 1);
-	set_var("TEXTEDITOR=/usr/bin/vim", &g_var, 1);
+	set_var_if_not_exist("TEXTEDITOR=/usr/bin/vim", &g_var, 1);
 	return (0);
 }
