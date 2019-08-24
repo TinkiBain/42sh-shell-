cf244e4bf81016f338e643fa010f24c6f6c19a39
Wispmother Talea <wtalea@il-b3.21-school.ru>
2019-08-24 15:46:41 +0300

    Norm undo

--- sources/readline/actions/undo.c
+++ sources/readline/actions/undo.c
@@ -6,69 +6,74 @@
 /*   By: wtalea <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/16 13:55:09 by wtalea            #+#    #+#             */
-/*   Updated: 2019/08/23 20:10:44 by wtalea           ###   ########.fr       */
+/*   Updated: 2019/08/24 15:46:09 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions.h"
 
-void		undo(t_line *line)
+static	void	undo_del(t_line *line, t_undo_item **undo)
+{
+	ft_bzero(line->str->s, line->str->len);
+	line->str->len = 0;
+	str_xaddback(line->str, (*undo)->string.s, (*undo)->string.len);
+	line->cpos = (*undo)->cpos;
+	if (*undo)
+	{
+		str_delete(&(*undo)->string);
+		free(*undo);
+	}
+}
+
+static	void	undo_else(t_line *line)
+{
+	ft_bzero(line->str->s, line->str->len);
+	line->str->len = 0;
+	str_xaddback(line->str, "", 0);
+	line->cpos = 0;
+}
+
+static	void	undo_go_history(t_line *line, int lenh)
+{
+	while (next_history_check(line))
+		;
+	while (lenh--)
+		previous_history_check(line);
+}
+
+static	void	undo_zero(t_line *line)
+{
+	ft_bzero(line->str->s, line->str->len);
+	line->str->len = 0;
+	str_xaddback(line->str, "", 0);
+	line->cpos = 0;
+}
+
+void			undo(t_line *line)
 {
 	t_undo_item		*undo;
 	t_string		*cp;
-	int				i;
 	t_list			*temp;
 
 	temp = NULL;
 	undo = NULL;
-	i = 0;
 	cp = line->str;
 	if ((undo = pop_undo_list(&line->undo)) != NULL)
 	{
-		i = undo->lenh;
-		while (next_history_check(line))
-			;
-		while (i--)
-			previous_history_check(line);
+		undo_go_history(line, undo->lenh);
 		if (!ft_strcmp(line->str->s, undo->string.s))
 		{
 			str_delete(&undo->string);
 			undo ? free(undo) : 1;
 			if ((undo = pop_undo_list(&line->undo)) == NULL)
 			{
-				ft_bzero(line->str->s, line->str->len);
-				line->str->len = 0;
-				str_xaddback(line->str, "", 0);
-				line->cpos = 0;
+				undo_zero(line);
 				return ;
 			}
-			i = undo->lenh;
-			while (next_history_check(line))
-				;
-			while (i--)
-				previous_history_check(line);
+			undo_go_history(line, undo->lenh);
 		}
-		ft_bzero(line->str->s, line->str->len);
-		line->str->len = 0;
-		str_xaddback(line->str, undo->string.s, undo->string.len);
-		line->cpos = undo->cpos;
-		if (undo)
-		{
-			str_delete(&undo->string);
-			free(undo);
-		}
-/*		if (line->undo && line->undo->next)
-		{
-			temp = line->undo->next;
-			del_undo_one(line->undo, 1);
-			line->undo = temp;
-		}*/
+		undo_del(line, &undo);
 	}
 	else
-	{
-		ft_bzero(line->str->s, line->str->len);
-		line->str->len = 0;
-		str_xaddback(line->str, "", 0);
-		line->cpos = 0;
-	}
+		undo_else(line);
 }
