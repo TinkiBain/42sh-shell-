a1fed1c67e43ac9f6ad8146f6cf9cfc7576d7096
ZaprAlex <ggwin-go@student.21-school.ru>
2019-08-25 23:10:37 +0300

    norme: call_exec.c and traverse_cmd.c; add fc to builtins;

--- sources/ast/traverse_ast/check_cmd.c
+++ sources/ast/traverse_ast/check_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/25 18:47:59 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/08/25 19:05:40 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/08/25 21:51:19 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,24 @@ static int	cmd_is_exist(const char *cmd)
 	return (0);
 }
 
+static int	check_cmd_errors(const char *cmd)
+{
+	struct stat		st;
+
+	if (stat(cmd, &st) == -1)
+		return (return_error(cmd, ": No such file or directory"));
+	if (S_ISDIR(st.st_mode))
+		return (return_error(cmd, ": Is a directory"));
+	else if (!S_ISREG(st.st_mode))
+		return (return_error(cmd, ": No such file or directory"));
+	else if (access(cmd, X_OK))
+		return (return_error(cmd, ": Permission denied"));
+	return (0);
+}
+
 int			check_cmd(const char *cmd)
 {
 	const char		*p;
-	struct stat		st;
 
 	p = cmd;
 	while (*p)
@@ -40,17 +54,7 @@ int			check_cmd(const char *cmd)
 		++p;
 	}
 	if (*p == '/')
-	{
-		if (stat(cmd, &st) == -1)
-			return (return_error(cmd, ": No such file or directory"));
-		if (S_ISDIR(st.st_mode))
-			return (return_error(cmd, ": Is a directory"));
-		else if (!S_ISREG(st.st_mode))
-			return (return_error(cmd, ": No such file or directory"));
-		else if (access(cmd, X_OK))
-			return (return_error(cmd, ": Permission denied"));
-	}
+		return (check_cmd_errors(cmd));
 	else
 		return (cmd_is_exist(cmd));
-	return (0);
 }
--- sources/ast/traverse_ast/is_builtin.c
+++ sources/ast/traverse_ast/is_builtin.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   is_builtin.c                                    :+:      :+:    :+:   */
+/*   is_builtin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/03 19:32:27 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/08/25 19:10:51 by ggwin-go         ###   ########.fr       */
+/*   Created: 2019/08/25 21:37:10 by ggwin-go          #+#    #+#             */
+/*   Updated: 2019/08/25 23:07:08 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ int			is_builtin(const char *cmd)
 			ft_strequ(cmd, "echo") || ft_strequ(cmd, "type") ||
 			ft_strequ(cmd, "setenv") || ft_strequ(cmd, "unsetenv") ||
 			ft_strequ(cmd, "hash") || ft_strequ(cmd, "env") ||
-			ft_strequ(cmd, "set") || ft_strequ(cmd, "unset"))
+			ft_strequ(cmd, "set") || ft_strequ(cmd, "unset") ||
+			ft_strequ(cmd, "fc"))
 		return (1);
 	return (0);
 }
