59bec660692784897a0a47c8f57b50b5db74f6b0
Derrick Wisoky <dwisoky@il-b2.21-school.ru>
2019-08-27 20:28:47 +0300

    fix newline

--- sources/ast/parser/parser_print_error.c
+++ sources/ast/parser/parser_print_error.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 13:19:46 by dwisoky           #+#    #+#             */
-/*   Updated: 2019/08/04 22:45:23 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/08/27 20:26:38 by dwisoky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 void		*parser_print_error(char *error)
 {
+	if (g_error_pars == 1)
+		return (NULL);
 	g_error_pars = 1;
 	ft_putstr_fd(PROJECT_NAME ": syntax error near unexpected token '", 2);
 	ft_putstr_fd(error, 2);
