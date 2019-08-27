a64723f12251c4635e3fe639d5b1386c0de936a6
Wispmother Talea <wtalea@il-a2.21-school.ru>
2019-08-27 15:33:31 +0300

    readline: work -vi 'b', Good Norm

--- sources/readline/actions/backward_word.c
+++ sources/readline/actions/backward_word.c
@@ -6,20 +6,20 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 07:17:32 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/08/25 18:09:22 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/08/27 14:27:40 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions.h"
 
-static int	is_delim(char c)
+static int		is_delim(char c)
 {
 	if (ft_isalnum(c))
 		return (0);
 	return (1);
 }
 
-void		backward_word(t_line *line)
+void			backward_word(t_line *line)
 {
 	int i;
 	int j;
--- sources/readline/actions/vi_backward_word.c
+++ sources/readline/actions/vi_backward_word.c
@@ -6,36 +6,66 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/15 10:48:27 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/08/26 20:43:30 by wtalea           ###   ########.fr       */
+/*   Updated: 2019/08/27 15:32:52 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "actions.h"
 
-static	void	alnum_go(char *str, int *i)
-{
-	while (*(str + *i) && ft_isalnum(*(str + *i)) && *i)
-		--(*i);
-}
-
 static	void	space_go(char *str, int *i)
 {
-	while (*(str + *i) && ft_isspace(*(str + *i)) && *i)
+	if (*i < 0)
+		*i = 0;
+	while (*i && *(str + *i) && ft_isspace(*(str + *i)))
 		--(*i);
-	if (ft_isprint(*(str + *i)) && !ft_isalnum(*(str + *i)))
-		while (*(str + *i) && (ft_isprint(*(str + *i)) &&
-					!ft_isalnum(*(str + *i))) && *i)
+	if ((*i) && (ft_isprint(*(str + (*i))) &&
+			!ft_isalnum(*(str + (*i))) && !ft_isspace(*(str + (*i)))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				(ft_isprint(*(str + (*i - 1))) &&
+			!ft_isalnum(*(str + (*i - 1)))) && !ft_isspace(*(str + (*i - 1))))
 			--(*i);
-	else if (ft_isalnum(*(str + *i)))
-		while (*(str + *i) && ft_isalnum(*(str + *i)) && *i)
+	else if ((*i) && ft_isalnum(*(str + (*i))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				ft_isalnum(*(str + (*i - 1))))
+			--(*i);
+}
+
+static	void	alnum_go(char *str, int *i)
+{
+	if (((*i - 1) >= 0) && ft_isspace(*(str + (*i - 1))))
+	{
+		--(*i);
+		space_go(str, i);
+	}
+	else if (((*i - 1) >= 0) && (ft_isprint(*(str + (*i - 1))) &&
+			!ft_isalnum(*(str + (*i - 1))) && !ft_isspace(*(str + (*i - 1)))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				(ft_isprint(*(str + (*i - 1))) &&
+			!ft_isalnum(*(str + (*i - 1))) && !ft_isspace(*(str + (*i - 1)))))
+			--(*i);
+	else if (((*i - 1) >= 0) && ft_isalnum(*(str + (*i - 1))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				ft_isalnum(*(str + (*i - 1))))
 			--(*i);
 }
 
 static	void	signs_go(char *str, int *i)
 {
-	while (*(str + *i) && (ft_isprint(*(str + *i)) &&
-				!ft_isalnum(*(str + *i))) && *i)
+	if (((*i - 1) >= 0) && ft_isspace(*(str + (*i - 1))))
+	{
 		--(*i);
+		space_go(str, i);
+	}
+	else if (((*i - 1) >= 0) && ft_isalnum(*(str + (*i - 1))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				ft_isalnum(*(str + (*i - 1))))
+			--(*i);
+	else if (((*i - 1) >= 0) && ft_isprint(*(str + (*i - 1))) &&
+		!ft_isalnum(*(str + (*i - 1))) && !ft_isspace(*(str + (*i - 1))))
+		while (((*i - 1) >= 0) && *(str + (*i - 1)) &&
+				(ft_isprint(*(str + (*i - 1))) &&
+			!ft_isalnum(*(str + (*i - 1))) && !ft_isspace(*(str + (*i - 1)))))
+			--(*i);
 }
 
 static	void	forward_go(char *str, int *cpos)
@@ -43,16 +73,20 @@ static	void	forward_go(char *str, int *cpos)
 	int		i;
 
 	i = *cpos;
-	if (ft_isspace(*(str + i)))
-		space_go(str, &i);
-	else if (ft_isalnum(*(str + i)))
-		alnum_go(str, &i);
-	else if (ft_isprint(*(str + i)) && !ft_isalnum(*(str + i)))
-		signs_go(str, &i);
-	*cpos = i;
+	if (i)
+	{
+		if (ft_isspace(*(str + i)))
+			space_go(str, &i);
+		else if (ft_isalnum(*(str + i)))
+			alnum_go(str, &i);
+		else if (ft_isprint(*(str + i)) && !ft_isalnum(*(str + i)) &&
+				!ft_isspace(*(str + i)))
+			signs_go(str, &i);
+		*cpos = i;
+	}
 }
 
-void		vi_backward_word(t_line *line)
+void			vi_backward_word(t_line *line)
 {
 	int i;
 	int j;
