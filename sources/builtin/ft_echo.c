f6330fb8aad38ce2ac952d69055983c5558be69e
itynol <getler91@bk.ru>
2019-08-27 17:02:30 +0300

    Hot fix echo -n

--- sources/builtin/ft_echo.c
+++ sources/builtin/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 12:31:45 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/08/27 15:24:06 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/08/27 17:01:03 by jterry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,11 +86,7 @@ static int	ft_writer_contr(const char **command, int i, int l, int j)
 			if (command[i][j + 1])
 				l = echo_hendler(command[i], &j);
 			if (l == 0)
-			{
-				write(1, "\033[7;1m%\033[0m", 12);
-				write(1, "\n", 1);
 				return (-1);
-			}
 			else if ((command[i][j] && command[i][j] == '\\'
 					&& !command[i][j + 1]) || l == -1)
 				write(1, "\\", 1);
@@ -120,14 +116,15 @@ int			ft_echo(const char **command)
 	}
 	if (write(1, command[i], 0))
 		print_error("echo: ", "write error: Bad file descriptor");
-	while (command[i][0] == '-')
+	while (command[i] && command[i][0] == '-')
 		if ((flag = ft_options_contr(command, &i)) == -1)
 			break ;
+	if (command[i] == NULL)
+		return (1);
 	while (command[i])
 		if (ft_writer_contr(command, i++, -12, 0) < 0)
 			return (0);
-	if (flag == 1)
-		write(1, "\033[7;1m%\033[0m", 12);
-	write(1, "\n", 1);
+	if (flag != 1)
+		write (1, "\n", 1);
 	return (1);
 }
