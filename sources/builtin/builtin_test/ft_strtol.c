4eaed504a49aa110cee26ce2e7ff37d2caf7f631
Wispmother Talea <wtalea@su-a2.21-school.ru>
2019-09-07 20:25:25 +0300

    del trash ft_strtol

--- sources/builtin/builtin_test/ft_strtol.c
+++ sources/builtin/builtin_test/ft_strtol.c
@@ -6,7 +6,7 @@
 /*   By: wtalea <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 14:27:14 by wtalea            #+#    #+#             */
-/*   Updated: 2019/09/07 16:37:39 by wtalea           ###   ########.fr       */
+/*   Updated: 2019/09/07 20:19:57 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,73 +15,3 @@
 #include <errno.h>
 #include "libft.h"
 
-static	int	ft_issupper(int ch)
-{
-	if (ch >= 65 && ch <= 90)
-		return (1);
-	return (0);
-}
-
-long		ft_strtol(const char *nptr, char **endptr, register int base)
-{
-	const char *s;
-	unsigned long acc;
-	int c;
-	unsigned long cutoff;
-	int neg = 0, any, cutlim;
-
-	acc = 0;
-	any = 0;
-	s = nptr;
-	c = *s;
-	while (ft_isspace(c))
-		c = *s++;
-	if (c == '-')
-	{
-		neg = 1;
-		c = *s++;
-	}
-	else if (c == '+')
-		c = *s++;
-	if ((base == 0 || base == 16) && c == '0' && (*s == 'x' || *s == 'X'))
-	{
-		c = s[1];
-		s += 2;
-		base = 16;
-	}
-	if (base == 0)
-		base = c == '0' ? 8 : 10;
-	cutoff = neg ? -(unsigned long)LONG_MIN : LONG_MAX;
-	cutlim = cutoff % (unsigned long)base;
-	cutoff /= (unsigned long)base;
-	while (1)
-	{
-		c = *s++;
-		if (ft_isdigit(c))
-			c -= '0';
-		else if (ft_isalpha(c))
-			c -= ft_issupper(c) ? 'A' - 10 : 'a' - 10;
-		else
-			break ;
-		if (c >= base)
-			break ;
-		if (any < 0 || acc > cutoff || (acc == cutoff && c > cutlim))
-			any = -1;
-		else
-		{
-			any = 1;
-			acc *= base;
-			acc += c;
-		}
-	}
-	if (any < 0)
-	{
-		acc = neg ? LONG_MIN : LONG_MAX;
-		errno = ERANGE;
-	}
-	else if (neg)
-		acc = -acc;
-	if (endptr != 0)
-		*endptr = (char *)(any ? s - 1 : nptr);
-	return (acc);
-}
