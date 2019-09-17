1775c591d90b0bbb4fbac455e7a7b89d709c68e6
ZaprAlex <ggwin-go@student.21-school.ru>
2019-09-17 18:26:24 +0300

    fix: remove redir_set/reset from call_subshell()

--- sources/ast/traverse_ast/call_subshell.c
+++ sources/ast/traverse_ast/call_subshell.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 14:06:19 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/09/16 20:21:07 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/09/17 18:25:37 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	call_subshell(char *str, int sep, char **env, t_pjobs *local)
 	av[0] = PROJECT_NAME;
 	av[1] = NULL;
 	local = jobs_startet(ft_xstrdup("pjobs_name_for_subshell"), sep);
-	redir_set();
 	if (pipe(pipefd) == -1)
 		exit(-1);
 	write(pipefd[1], str, ft_strlen(str));
@@ -42,6 +41,5 @@ void	call_subshell(char *str, int sep, char **env, t_pjobs *local)
 		ft_waitpid(pid);
 	else
 		ft_printf("[%d] [%d]\n", local->num, pid);
-	redir_reset();
 	set_result();
 }
