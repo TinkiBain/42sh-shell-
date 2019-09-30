5426ca2f090eed6ce2f1cad2fd2060c12ead1c4e
ZaprAlex <ggwin-go@student.21-school.ru>
2019-09-30 20:29:58 +0300

    case (script) without globbing done

--- sources/ast/traverse_ast/traverse_case_clause.c
+++ sources/ast/traverse_ast/traverse_case_clause.c
@@ -6,23 +6,34 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 19:07:01 by ggwin-go          #+#    #+#             */
-/*   Updated: 2019/09/29 21:24:36 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/09/30 20:27:52 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-void	traverse_case_clause(t_case_clause *list, t_pjobs *local)
+void			traverse_case_clause(t_case_clause *list, t_pjobs *local)
 {
-	printf("%s\n", list->word);
-	(void)local;
-	// while (list->case_list)
-	// if (list->word)
-	// 	while (42)
-	// 	{
-	// 		traverse_compound_list(list->case_list->case_item->compound_list, local);
-	// 		if (g_res_exec)
-	// 			break ;
-	// 		traverse_compound_list(list->word, local);
-	// 	}
+	t_case_list	*elem;
+	t_pattern	*pattern;
+	char		*target;
+
+	list->word = tdq(list->word);
+	elem = list->case_list;
+	target = list->word;
+	while (elem)
+	{
+		pattern = elem->case_item->pattern;
+		while (pattern)
+		{
+			if (ft_strequ(target, pattern->word) ||
+				(ft_strequ(pattern->word, "*")))
+			{
+				traverse_compound_list(elem->case_item->compound_list, local);
+				return ;
+			}
+			pattern = pattern->next_pattern;
+		}
+		elem = elem->next;
+	}
 }
