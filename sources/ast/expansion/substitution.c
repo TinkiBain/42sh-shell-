8c3f4b2d031f0b181b9d23cc75126d2c8ba88ba5
Derrick Wisoky <dwisoky@ga-c5.21-school.ru>
2019-10-07 22:40:00 +0300

    fix substitution

--- sources/ast/expansion/substitution.c
+++ sources/ast/expansion/substitution.c
@@ -3,59 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   substitution.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jterry <jterry@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dwisoky <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 20:58:11 by dwisoky           #+#    #+#             */
-/*   Updated: 2019/10/07 21:50:50 by jterry           ###   ########.fr       */
+/*   Updated: 2019/10/07 22:30:27 by dwisoky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh.h"
 
-char		*substitution_write(int pipefd[2], int tmpfd)
+char		*substitution_write(int pipefd[2])
 {
 	char	buf[1024];
 	char	*tmp;
 	int		i;
 
 	tmp = ft_strnew(1);
-	close(pipefd[1]);
 	while ((i = read(pipefd[0], buf, 1023)))
 	{
 		buf[i] = '\0';
 		tmp = ft_strrejoin(tmp, buf, 1);
 	}
 	close(pipefd[0]);
-	dup2(tmpfd, 1);
 	tmp[ft_strlen(tmp) - 1] = '\0';
 	return (tmp);
 }
 
 char		*substitution_exec(char *str)
 {
-	extern char		*g_buf;
+	pid_t			pid;
 	t_complete_cmd	*parser_list;
 	int				pipefd[2];
-	int				tmpfd;
 	t_lex			*lex;
-	t_lex			*tmp;
-	char			*src;
 
 	if (pipe(pipefd) < 0)
-		return (NULL);
-	tmpfd = dup(1);
-	dup2(pipefd[1], 1);
-	lex = NULL;
-	tmp = g_lex;
-	src = g_buf;
-	parser_list = exec_ast(str, &lex);
-	if (parser_list)
-		traverse_ast(parser_list);
-	parser_free(parser_list);
-	lexer_free_all(lex);
-	g_lex = tmp;
-	g_buf = src;
-	return (substitution_write(pipefd, tmpfd));
+		exit(-1);
+	if (!(pid = fork()))
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], 1);
+		lex = NULL;
+		parser_list = exec_ast(str, &lex);
+		if (parser_list)
+			traverse_ast(parser_list);
+		parser_free(parser_list);
+		lexer_free_all(lex);
+		exit(0);
+	}
+	close(pipefd[1]);
+	waitpid(pid, NULL, 0);
+	return (substitution_write(pipefd));
 }
 
 char		*substitution(char *str)
@@ -66,7 +63,7 @@ char		*substitution(char *str)
 	if (*str == '$')
 	{
 		begin += 2;
-		str = parser_find_dollar(str);
+		str = lexer_find_dollar(str);
 	}
 	else
 	{
