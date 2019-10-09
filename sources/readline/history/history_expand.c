0a0c647c7b98f3fc74cde73b1b57301561d2a063
Gormlaith Melisandre <gmelisan@student.21-school.ru>
2019-10-09 19:46:56 +0300

    hotfix: doubling command

--- sources/readline/history/history_expand.c
+++ sources/readline/history/history_expand.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 13:54:22 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/10/09 19:03:59 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/10/09 19:45:55 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,22 @@ int				history_expand(t_line *line)
 	int		i;
 	char	c;
 	int		res;
+	int		first;
 
 	res = 1;
 	i = -1;
-	move_cur_nl();
+	first = 1;
 	while ((c = str_get(*line->str, ++i)))
 	{
 		if (c == '!')
+		{
+			if (first)
+				move_cur_nl();
 			if (!(res = history_expand_one(line, &i)))
 				break ;
+		}
+		if (first)
+			first = 0;
 	}	
 	return (res);
 }
