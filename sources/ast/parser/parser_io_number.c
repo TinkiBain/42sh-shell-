fee1bfab9dacf967a47de2e62795def52c8f69a0
Derrick Wisoky <dwisoky@mi-c2.21-school.ru>
2019-10-10 16:39:38 +0300

    fix parser_io_number

--- sources/ast/parser/parser_io_number.c
+++ sources/ast/parser/parser_io_number.c
@@ -6,7 +6,7 @@
 /*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/15 20:13:28 by dwisoky           #+#    #+#             */
-/*   Updated: 2019/09/23 14:15:57 by ggwin-go         ###   ########.fr       */
+/*   Updated: 2019/10/09 21:48:55 by dwisoky          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,22 @@
 int		parser_io_number(void)
 {
 	int	tmp;
+	int	nb;
+	int	i;
 
-	//добавить проверку преполнения
-	tmp = ft_atoi(g_lex->lexem);
+	nb = 0;
+	i = 0;
+	while (g_lex->lexem[i] >= '0' && g_lex->lexem[i] <= '9')
+	{
+		tmp = nb * 10 + (g_lex->lexem[i] - '0');
+		if (tmp < nb)
+		{
+			nb = -2;
+			break ;
+		}
+		nb = tmp;
+		++i;
+	}
 	g_lex = g_lex->next;
-	return (tmp);
+	return (nb);
 }
