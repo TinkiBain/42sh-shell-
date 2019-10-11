34679be1c051cf19321b3906829ccd719b92109d
Gormlaith Melisandre <gmelisan@student.21-school.ru>
2019-10-11 18:01:15 +0300

    Hotfix in str_delarr

--- libft/sources/ft_string/str_delarr.c
+++ libft/sources/ft_string/str_delarr.c
@@ -6,7 +6,7 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 17:10:04 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/03/05 17:12:39 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/10/11 17:58:22 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ void	str_delarr(t_string **strarr)
 	int i;
 
 	i = 0;
-	while ((*strarr)[i].s)
-		str_delete(&(*strarr)[i++]);
-	free(*strarr);
-	*strarr = NULL;
+	if (strarr && *strarr)
+	{
+		while ((*strarr)[i].s)
+			str_delete(&(*strarr)[i++]);
+		free(*strarr);
+		*strarr = NULL;
+	}
 }
