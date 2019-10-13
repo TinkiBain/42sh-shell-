2a28872634d14a7f9d6eb778afabd67b3ec85257
Wispmother Talea <wtalea@ga-b4.21-school.ru>
2019-10-13 18:32:23 +0300

    fix leaks in ft_fc_edit_run_editor

--- sources/builtins/ft_fc/ft_fc_edit_run_editor.c
+++ sources/builtins/ft_fc/ft_fc_edit_run_editor.c
@@ -6,7 +6,7 @@
 /*   By: jterry <jterry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 20:32:41 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/10/11 15:06:47 by jterry           ###   ########.fr       */
+/*   Updated: 2019/10/13 18:16:38 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,19 @@
 int			ft_fc_edit_run_editor(char **argv)
 {
 	extern char		**environ;
-	t_string		name;
 	pid_t			pid;
 
-	name = str_xcopy(argv[0]);
-	str_xaddback(&name, " ", 1);
-	str_xaddback(&name, argv[1], ft_strlen(argv[1]));
 	pid = fork();
 	if (pid == 0 && execve(argv[0], argv, environ) < 0)
 	{
-		loginfo("ft_fc(): execve error (%s)", name.s);
+		loginfo("ft_fc(): execve error (%s %s)", argv[0], argv[1]);
 		return (1);
 	}
 	else if (pid > 0)
 		ft_waitpid(pid);
 	else
 	{
-		loginfo("ft_fc(): fork error (%s)", name.s);
+		loginfo("ft_fc(): fork error (%s %s)", argv[0], argv[1]);
 		return (1);
 	}
 	return (g_res_exec);
