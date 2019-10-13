9f131f1e2b1083d08a3e6aa6d3a283ba4f716102
Wispmother Talea <wtalea@ga-b4.21-school.ru>
2019-10-13 15:14:07 +0300

    del extern in alias_func_size.c

--- sources/ast/alias_func_size.c
+++ sources/ast/alias_func_size.c
@@ -6,14 +6,12 @@
 /*   By: wtalea <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 16:43:31 by wtalea            #+#    #+#             */
-/*   Updated: 2019/10/13 12:23:12 by wtalea           ###   ########.fr       */
+/*   Updated: 2019/10/13 15:12:47 by wtalea           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hash.h"
 #include "dictionary.h"
-
-extern	t_alias	*g_alias;
 
 int		func_size_list(t_dict *func)
 {
