1f40d82d8ffa9e6dccd3761349648899f6b6a582
ZaprAlex <ggwin-go@student.21-school.ru>
2019-10-23 15:48:45 +0300

    print_error() fixed, all message printed by single write

--- sources/error/print_error.c
+++ sources/error/print_error.c
@@ -3,16 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jterry <jterry@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ggwin-go <ggwin-go@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 22:35:39 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/10/19 21:12:00 by jterry           ###   ########.fr       */
+/*   Updated: 2019/10/23 15:46:50 by ggwin-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "xfuncs.h"
 #include "error.h"
 #include "sem.h"
+
+extern t_opt	g_opt;
+extern int		g_line_num;
 
 static t_string	format_info(const char *info)
 {
@@ -38,24 +41,52 @@ static t_string	format_info(const char *info)
 	return (str);
 }
 
+// void			print_error(const char *info, const char *msg)
+// {
+// 	extern t_opt	g_opt;
+// 	extern int		g_line_num;
+// 	t_string		info_formatted;
+
+// 	reserve_sem(SEMPRINT, 1);
+// 	ft_fdprintf(STDERR, "%s: ", g_project_name);
+// 	if (g_opt.rl_gnl != 0)
+// 		ft_fdprintf(STDERR, "line %d: ", g_line_num);
+// 	if (info)
+// 	{
+// 		info_formatted = format_info(info);
+// 		ft_fdprintf(STDERR, "%s: ", info_formatted.s);
+// 		str_delete(&info_formatted);
+// 	}
+// 	ft_fdprintf(STDERR, "%s\n\r", msg);
+// 	loginfo("! Error: %s (\"%s\")", msg, info);
+// 	release_sem(SEMPRINT, 1);
+// }
+
 void			print_error(const char *info, const char *msg)
 {
-	extern t_opt	g_opt;
-	extern int		g_line_num;
-	t_string		info_formatted;
+	t_string	info_formatted;
+	char		*tmp;
 
 	reserve_sem(SEMPRINT, 1);
-	ft_fdprintf(STDERR, "%s: ", g_project_name);
+	tmp = ft_xstrjoin(g_project_name, ": ");
 	if (g_opt.rl_gnl != 0)
-		ft_fdprintf(STDERR, "line %d: ", g_line_num);
+	{
+		tmp = ft_xstrrejoin(tmp, "line ", 1);
+		tmp = ft_xstrrejoin(tmp, ft_itoa(g_line_num), 2);
+		tmp = ft_xstrrejoin(tmp, ": ", 1);
+	}
 	if (info)
 	{
 		info_formatted = format_info(info);
-		ft_fdprintf(STDERR, "%s: ", info_formatted.s);
+		tmp = ft_xstrrejoin(tmp, info_formatted.s, 1);
+		tmp = ft_xstrrejoin(tmp, ": ", 1);
 		str_delete(&info_formatted);
 	}
-	ft_fdprintf(STDERR, "%s\n\r", msg);
+	tmp = ft_xstrrejoin(tmp, msg, 1);
+	tmp = ft_xstrrejoin(tmp, "\n\r", 1);
+	ft_putstr_fd(tmp, STDERR);
 	loginfo("! Error: %s (\"%s\")", msg, info);
+	ft_strdel(&tmp);
 	release_sem(SEMPRINT, 1);
 }
 
@@ -69,9 +100,7 @@ void			print_error_exit(const char *info, const char *msg,
 
 void			print_error_vaarg(const char *msg, ...)
 {
-	va_list			ap;
-	extern t_opt	g_opt;
-	extern int		g_line_num;
+	va_list		ap;
 
 	reserve_sem(SEMPRINT, 1);
 	ft_fdprintf(STDERR, "%s: ", g_project_name);
