e34b0a706f574a46c74662a7c1da07e3171000ca
Gormlaith Melisandre <gmelisan@ga-a1.21-school.ru>
2019-11-01 19:58:15 +0300

    history_expansion (!) disable inside [], (), "", ''

--- sources/readline/history/history_expand.c
+++ sources/readline/history/history_expand.c
@@ -6,19 +6,35 @@
 /*   By: gmelisan <gmelisan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/22 13:54:22 by gmelisan          #+#    #+#             */
-/*   Updated: 2019/10/13 12:37:25 by gmelisan         ###   ########.fr       */
+/*   Updated: 2019/11/01 19:57:15 by gmelisan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "history.h"
 
+static char		get_stop(char c)
+{
+	if (c == '[')
+		return (']');
+	if (c == '(')
+		return (')');
+	if (c == '\'')
+		return (c);
+	if (c == '"')
+		return (c);
+	return (0);
+}
+
 static void		skip(t_line *line, int *i)
 {
 	char	c;
+	char	stop;
 
+	stop = get_stop(str_get(*line->str, *i));
+	*i += 1;
 	while ((c = str_get(*line->str, *i)))
 	{
-		if (c == ']')
+		if (c == stop)
 			break ;
 		*i += 1;
 	}
@@ -36,7 +52,7 @@ int				history_expand(t_line *line)
 	first = 1;
 	while ((c = str_get(*line->str, ++i)))
 	{
-		if (c == '[')
+		if (ft_strchr("[('\"", c))
 			skip(line, &i);
 		else if (c == '!' &&
 				!(res = history_expand_one(line, &i, &first)))
